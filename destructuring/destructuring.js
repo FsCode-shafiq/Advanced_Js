34d98fe504229904104cc0cf72df27fd55cb247c
FS.Code*shafiq <shafiqurrehman45@yahoo.com>
2020-09-15 22:55:00 +0500

    destructuring

--- destructuring/destructuring.js
+++ destructuring/destructuring.js
@@ -66,3 +66,84 @@ const {profession:{one,two}} = obj;
 
 console.log(two);*/
 
+
+
+
+// ***EXAMPLE NO-3 (--NESTED DESTRUCTURING--)***
+/* 
+const obj = {
+    firstName: 'abdul',
+    LastName: 'wassay',
+    age: 20,
+    profession: {
+        one: 'React',
+        two: 'Node-js',
+        three: {
+            four: 'express',
+            five: 'mongoDB'
+        }
+    },
+
+};
+
+const {profession:{one,two,three}} = obj;
+
+console.log(three);*/
+
+
+// ***EXAMPLE NO-4 (--NESTED DESTRUCTURING--)***
+/* 
+const obj = {
+    firstName: 'abdul',
+    LastName: 'wassay',
+    age: 20,
+    profession: {
+        one: 'React',
+        two: 'Node-js',
+        three: {
+            four: 'express',
+            five: 'mongoDB'
+        }
+    },
+
+};
+
+const {profession:{one,two,three:{four,five}}} = obj;
+
+console.log(five);*/
+
+
+// ***EXAMPLE NO-2 (--NESTED DESTRUCTURING--)***
+ /*
+const obj = {
+    firstName: 'abdul',
+    LastName: 'wassay',
+    age: 20,
+    profession: {
+        one: 'React',
+        two: 'Node-js',
+        three: {
+            four: 'express',
+            five: 'mongoDB'
+        }
+    },
+
+};
+
+const {profession:{one,two,three:{four,five}}} = obj;
+
+console.log(three);
+*/
+
+
+
+//          ***Array Destructuring***
+
+/*
+const array = [1,2,3,4,5,6,7,8,9,10];
+
+const [one,two,three,four,five,six,seven,eight,nine,ten] = array;
+
+console.log(five);
+
+*/
