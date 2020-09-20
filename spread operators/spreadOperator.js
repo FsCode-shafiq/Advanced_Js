945196feec2decf080d8c8bca862d1f4abfed9b3
FS.Code*shafiq <shafiqurrehman45@yahoo.com>
2020-09-20 05:40:19 +0500

    Update spreadOperator.js

--- spread operators/spreadOperator.js
+++ spread operators/spreadOperator.js
@@ -2,7 +2,11 @@
 
 let obj1 = {
     fname: 'Abdul',
-    lname: 'wassay'
+    lname: 'wassay',
+    adress:{
+        place1: 'karachi',
+        place2: 'lahore'
+    }
 };
 
 // let obj2 = obj1;
@@ -18,12 +22,34 @@ let obj1 = {
  * is cheez se bachny k liye hum spread operator use krne hain
  */
 
-var obj2 = {
-    ...obj1
-}
+// var obj2 = {
+//     ...obj1
+// }
 
-obj2.fname = 'muhammad';
+// obj2.fname = 'muhammad';
 
-console.log('object 1 ====> ',obj1);
+// console.log('object 1 ====> ',obj1);
 
-console.log('object 2 ====> ',obj2);
+// console.log('object 2 ====> ',obj2);
+
+
+/* hum spread krty waqt bhi value change kr skty hain*/
+
+
+// var obj2 = {
+//     ...obj1,
+//     lname: 'raheem'
+// };
+
+// console.log('object 1 ====> ',obj1);
+
+// console.log('object 2 ====> ',obj2);
+
+// const obj2 = {
+//     ...obj1,
+//     adress:{
+//         ...obj1.adress,
+//         place1:'Islamabad'
+//     }
+// }
+// console.log('object :>> ', obj2);
