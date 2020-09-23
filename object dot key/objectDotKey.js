8a527d5dfb6820b2c60a14457a0bfe31a13be2ec
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2020-09-23 12:32:03 +0500

    objet dot key

---
+++ object dot key/objectDotKey.js
@@ -0,0 +1,15 @@
+/**
+ * object dot key hum is liye use krty hain Q k agr hum ko object ki key ki madad se value leni ho tu wo humary liye asaan ho jati hain 
+ * object dot ki humen ek array bana kr de deti hain kis bhji object k sari keys ka
+ */
+
+let myobj={
+    name: 'shafiq',
+    age: 25,
+    gender: 'male',
+    phone: '03442078398'
+};
+
+let objDotKey = Object.keys(myobj);
+
+console.log(objDotKey);
