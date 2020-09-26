1f01d6940fa889f3fdde2a940960aadddf3b39e6
FS.Code*shafiq <shafiqurrehman45@yahoo.com>
2020-09-26 10:40:04 +0500

    Create objectDotAssign.js

---
+++ object dot assign/objectDotAssign.js
@@ -0,0 +1,47 @@
+/**
+ * kisi bhi object ko dusary object k ander copy krne k do tariqy hain (bina refference pass krwaey)
+ * ek spread operator k through 
+ * aur dusara object dot assign k through
+ * pehle hum spread operator k thorugh dekh lete hain phr object dot assign k throgh dekhen gaey
+ */
+
+
+ let obj1 = {
+     firstName:'Abdul'
+ }
+
+ let obj2 = {
+     lastName: 'wassay'
+ }
+
+ // *** Spread Operator ***
+
+ let spreadObj = {
+     ...obj1,
+     ...obj2
+ }
+
+ //console.log(spreadObj);
+
+// check assignment
+
+//spreadObj.lastName = 'Raheem';
+
+
+//console.log(obj2.lastName);
+
+
+//console.log(spreadObj);  // work perfectly
+
+
+// *** Object Dot Assign ***
+
+
+let dotAssignObj = Object.assign({},obj1,obj2);
+
+obj1.firstName = 'muhammad';
+
+console.log(obj1);
+
+console.log(dotAssignObj);
+
