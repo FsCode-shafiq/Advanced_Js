55acde8d4e2a7cb1aa3973eda1885498b7b12c94
FS.Code*shafiq <shafiqurrehman45@yahoo.com>
2020-09-26 13:12:38 +0500

    Create restOperator.js

---
+++ Rest operator/restOperator.js
@@ -0,0 +1,26 @@
+/**
+ * rest operator hum destructure krty hue use krty hain jb hum ko koi value destructure krni ho
+ * aur baqi ki value humen kisi variable me store krwani ho tu
+ */
+
+const { lightcoral } = require("color-name")
+
+ let obj = {
+     firstName: 'shafiq',
+     lastName: 'ur rehman',
+     city: 'khi',
+     address: {
+         add1:'malir',
+         add2: 'korangi',
+         add3: 'saddar'
+     }
+ }
+
+// let {city, ...restValue} = obj;
+// console.log(city,restValue);
+
+// **** TASK NO1 (NESTED VALUES RESTING) ****
+
+let {city , address:{add1, ...restAdd},...restValue}= obj;
+console.log(restAdd,city,restValue);
+
