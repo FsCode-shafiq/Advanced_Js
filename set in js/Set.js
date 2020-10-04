f0b145a29c805ea14049f98b8ff8269c80b5f8b5
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2020-10-04 19:30:58 +0500

    Create Set.js

---
+++ set in js/Set.js
@@ -0,0 +1,17 @@
+/**
+ * javascript agr hum kisi array me unique value rkhna chaty hain means k hr value dusary se algh ho tu hum ousko set me rkh kr define kr skty hain,
+ */
+
+let arr = [1, 2, 2, 3, 4, 5, 6];
+
+const setArr = new Set(arr);
+
+console.log(setArr)
+
+const newSet = new Set();
+
+newSet.add("shafiq");
+newSet.add("shafiq");
+newSet.add("shafiq");
+
+console.log(newSet);
