8c38c767334ca5bfc9e9b5bf987584103e9e8868
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2021-08-28 17:39:02 +0500

    ....

--- ex.js
+++ ex.js
@@ -1,2 +1,13 @@
 a = this;
 console.log(this);
+
+function greeting(name) {
+    alert('Hello ' + name);
+  }
+  
+  function processUserInput(callback) {
+    var name = prompt('Please enter your name.');
+    callback(name);
+  }
+  
+  processUserInput(greeting);
